7e677d0e9fa7b5f59b31a159fc111edcdc9559e8
eldordev19 <eldordev19@gmail.com>
2023-08-14 15:05:44 +0500

    first commit

---
+++ src/main/java/uz/rtmc/contractapp/ContractappApplication.java
@@ -0,0 +1,13 @@
+package uz.rtmc.contractapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/rtmc/contractapp/ContractappApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.rtmc.contractapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
